8c91979f7ec9240b05ced3840de1da06aefdf67f
KalaGemet <65995084+kalagemet@users.noreply.github.com>
2026-05-03 10:29:06 +0700

    Update beritaDetailNew.php
    Delete duplicat meta desc

--- app/Views/publik/beritaDetailNew.php
+++ app/Views/publik/beritaDetailNew.php
@@ -16,8 +16,7 @@
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:image" content="<?= base_url('uploads/' . $berita['gambar']) ?>" />
 <meta property="og:title" content="<?= $berita['judul'] ?? '' ?>" />
-<meta property="og:description" content="<?= $berita['judul'] ?? '' ?>" />
-<meta property="og:description" content="<?= $meta_desc ?? '' ?>" />
+<meta property="og:description" content="<?= $meta_desc ?? $berita['judul'] ?>" />
 <meta property="og:image" content="<?= base_url('uploads/' . $berita['gambar']) ?>" />
 <meta property="og:image" content="<?= base_url('uploads/' . 'thumbnail_' . $berita['gambar']) ?>" />
 <meta property="og:url" content="<?= current_url(true); ?>" />
